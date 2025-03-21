69e33f7af8bf549b24976ef0f1832776114cebaf
kyssiei <kyssiei.kay@gmail.com>
2025-03-20 21:57:48 -0400

    updated code and fixed image downloader functionality

--- index.js
+++ index.js
@@ -6,20 +6,31 @@ const morgan = require('morgan')
 
 const aboutRoute = require('./routes/about')
 const greetRoute = require('./routes/greet')
+const imageRoute = require('./routes/image')
 
-app.use(bodyParser.urlencoded({exteded: true}))
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
 //setting the view engine as ejs
 app.set('view engine', 'ejs')
 
-app.use('/about', aboutRoute)
+app.use(express.static('images'))
 
+app.use('/about', aboutRoute)
+app.use('/greet', greetRoute)
+app.use('/image', imageRoute)
 
 app.get('/', (req, res) => {
-    res.send("this is our homepage")
+    res.render('index', {title: "Home"})
 })
+
+app.post('/submit-form',(req, res) =>{
+    console.log(req.body);
+    res.send("Form submitted successfully")
+    
+})
+
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
     
